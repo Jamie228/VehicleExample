8c2fe714cdb7ff376c86d7338b8aff441136d478
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-10-04 14:12:24 +0100

    Init DotNet Core Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
