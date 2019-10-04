e3b86152b61d182e5102c20aaf3d02f24fe8425d
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-10-04 14:20:50 +0100

    Created Classes + Methods

--- Program.cs
+++ Program.cs
@@ -2,6 +2,42 @@
 
 namespace VehicleExample
 {
+    public class Vehicle
+    {
+        public string make = "";
+        public string model = "";
+        public struct position
+        {
+            public int posx;
+            public int posy;
+        }
+        public void MoveForward()
+        {
+
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public int numberOfWheels;
+        public int currentGear;
+
+        public void Reverse()
+        {
+
+        }
+    }
+
+    public class Motorbike : Vehicle
+    {                
+        public int numberOfWheels;
+        public int currentGear;
+
+        public void Wheelie()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
